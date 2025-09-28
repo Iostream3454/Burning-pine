668317e6409c371657a29002432d303b6c86d294
Iostream3454 <filosof868@gmail.com>
2025-09-28 16:29:48 +0300

    add FPS limits

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,15 +3,16 @@
 Application::Application() : window_(sf::VideoMode({ 200, 200 }), "SFML Works"), gui_(window_)//подключение gui к окну
 {
 	window_.setVerticalSyncEnabled(true);//вертикальная синхронизация
+	window_.setFramerateLimit(60);
 }
 
 void Application::runApplication()
 {
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Cyan);
-	IScene* tScene = new MenuScene;
-	IScene* tGScene = new GameScene;
-	sceneManager_.changeScene(*tScene);
+	IScene* tScene = new MenuScene();
+	IScene* tGScene = new GameScene();
+	sceneManager_.changeScene(tScene);
 	while (window_.isOpen()) {//основной цикл
 		timeManager_.updateTime();
 		while (const std::optional event = window_.pollEvent())
