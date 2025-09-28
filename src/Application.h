e95f2015c9cff43bc7f0ce7e2b7c91ce0bce31be
Iostream3454 <filosof868@gmail.com>
2025-09-28 15:10:39 +0300

    Imlement sceneManager in Application

--- src/Application.h
+++ src/Application.h
@@ -3,6 +3,7 @@
 #include <TGUI/TGUI.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
 #include "TimeManager.h"
+#include "SceneManager.h"
 
 class Application {//этот класс является точкой создания оконного приложения 
 public:
@@ -14,4 +15,5 @@ private:
 	sf::RenderWindow window_; //переменная окна
 	tgui::Gui gui_;//переменная для подключения TGUI
 	TimeManager timeManager_;
+	SceneManager sceneManager_;//менеджер по переключению сцен
 };
