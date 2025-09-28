6f63b869879ca8bc7e967dd4b8fc9133f22a8c7f
Iostream3454 <filosof868@gmail.com>
2025-09-28 16:56:00 +0300

    add final version of prototype of SceneManager

--- src/Application.cpp
+++ src/Application.cpp
@@ -15,14 +15,26 @@ void Application::runApplication()
 	sceneManager_.changeScene(tScene);
 	while (window_.isOpen()) {//основной цикл
 		timeManager_.updateTime();
-		while (const std::optional event = window_.pollEvent())
+		while (const std::optional<sf::Event> ev = window_.pollEvent())
 		{
-			gui_.handleEvent(*event);
+			gui_.handleEvent(*ev);
 
-			if (event->is<sf::Event::Closed>())
-				window_.close();
+			if (ev->is<sf::Event::Closed>())
+			{
+				window_.close(); 
+			}
+			else if (const auto * key = ev->getIf<sf::Event::KeyPressed>())
+			{
+				if(key->scancode == sf::Keyboard::Scancode::Q)
+				{
+					if (sceneManager_.getCurrScene() == tScene)  sceneManager_.changeScene(tGScene);
+					else if (sceneManager_.getCurrScene() == tGScene)  sceneManager_.changeScene(tScene);
+				}else if(key->scancode == sf::Keyboard::Scancode::Escape) window_.close();
+					
+			}
 		}
 		float dt = timeManager_.getDeltaTime();
 		sceneManager_.updateScene(dt);
 	}
+	delete tScene; delete tGScene;
 }
