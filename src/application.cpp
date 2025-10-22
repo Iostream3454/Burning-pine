f50a8a0430d45efb5ee5abe44df6b33dfa901b9d
Iostream3454 <filosof868@gmail.com>
2025-10-22 20:15:10 +0300

    make sceneManager

--- src/application.cpp
+++ src/application.cpp
@@ -1,20 +1,20 @@
 #include "application.h"
 #include "mainMenuScene.h"
+#include "sceneManager.h"
 #include<memory>
 
 void Application::run() {
-	//MainMenuScene mmScene;
-	std::unique_ptr<IScene> mm = std::make_unique<MainMenuScene>();
-	mm->ready(); float dt = float(1.0 / 60);
+	SceneManager::push(std::move(std::make_unique<MainMenuScene>()));
+	float dt = float(1.0 / 60);
 	while (mWindow.isOpen()) {
 		while (const std::optional<sf::Event> event = mWindow.pollEvent()) {
 			if (event->is<sf::Event::Closed>()) Window::close(); //close window in singleton
 
-			mm->handlerEvent(*event);
+			SceneManager::handlerEvent(*event);
 		}
-		mm->update(dt);
+		SceneManager::update(dt);
 		mWindow.clear(sf::Color::White);
-		mm->render(mWindow);
+		SceneManager::render(mWindow);
 
 	}
 
---
+++ src/sceneManager.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include<stack>
+
+#include "sceneInterface.h"
+#include "mainMenuScene.h"
+
+class SceneManager {
+public:
+
+	SceneManager(const SceneManager&) = delete;
+	SceneManager& operator=(const SceneManager&) = delete;
+	SceneManager(SceneManager&&) = delete;
+	SceneManager& operator=(SceneManager&&) = delete;
+
+	static void push(std::unique_ptr<IScene> scene) {
+		mSceneStack.push(std::move(scene));
+		mSceneStack.top()->ready();
+	}
+
+	static void pop() {
+		if (mSceneStack.empty()) return;
+		mSceneStack.pop();
+		mSceneStack.top()->ready();
+	}
+
+	static void ready() {
+		if (mSceneStack.empty()) return;
+		mSceneStack.top()->ready();
+	}
+
+	static void handlerEvent(const sf::Event& ev) {
+		if (mSceneStack.empty()) return;
+		mSceneStack.top()->handlerEvent(ev);
+	}
+
+	static void update(float& dt) {
+		if (mSceneStack.empty()) return;
+		mSceneStack.top()->update(dt);
+	}
+
+	static void render(sf::RenderWindow& win) {
+		if (mSceneStack.empty()) return;
+		mSceneStack.top()->render(win);
+	}
+
+private:
+	SceneManager() {};
+
+	~SceneManager() = default;
+	static std::stack<std::unique_ptr<IScene>> mSceneStack;
+};
+
+std::stack<std::unique_ptr<IScene>> SceneManager::mSceneStack = {};
