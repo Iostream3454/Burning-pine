108949a223c9664758e6b0a2c671cec8931efada
Iostream3454 <filosof868@gmail.com>
2025-10-28 14:03:47 +0300

    add:: add character in gameScene

--- src/gameScene.cpp
+++ src/gameScene.cpp
@@ -8,21 +8,30 @@ void GameScene::ready() {
 	butt->setHeight(50);
 	butt->setWidth(150);
 	mGuiLayer.add(butt, "mainMenu");
+	mCharacter.setPosition({ 200.f, 200.f });
 }
 
 void GameScene::handlerEvent(const sf::Event& ev) {
-	mGuiLayer.handleEvent(ev);
+	bool guiClick = mGuiLayer.handleEvent(ev);
+	
 	if (mGuiLayer.get("mainMenu").get()->isMouseDown()) {
 		mGuiLayer.removeAllWidgets();
 		SceneManager::push(std::move(std::make_unique<MainMenuScene>()));
+	}
+	if(!guiClick)
+	{
+		mCharacter.setPositionGoal(ev);
 	}
 }
 
 void GameScene::render(sf::RenderWindow& win) {
 	mGuiLayer.draw();
+	win.draw(mCharacter);
 	win.display();
 }
 
 void GameScene::update(float& dt) {
-
+	if (mCharacter.getHasGoal()) {
+		mCharacter.move();
+	}
 }
