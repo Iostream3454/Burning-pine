e86a766aae625b28e2d693883515c0b435f44820
Iostream3454 <filosof868@gmail.com>
2025-11-01 20:38:11 +0300

    add: change double on float for mTargetFps

--- src/application.h
+++ src/application.h
@@ -8,6 +8,6 @@ public:
 private:
 	sf::RenderWindow& mWindow = Window::instance();
 	sf::Clock mClock; 
-	double mTargetFps = 60.0;
+	float mTargetFps = 60.0;
 	sf::Time mTargetTps = sf::seconds((1.f / mTargetFps));
 };
