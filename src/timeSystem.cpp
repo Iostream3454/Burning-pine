9b165edbbf03c73f9cbf01572c7c19882b0c2af8
Iostream3454 <filosof868@gmail.com>
2025-11-09 11:19:45 +0300

    feat: add hour pass return

--- src/timeSystem.cpp
+++ src/timeSystem.cpp
@@ -1,15 +1,16 @@
 #include <string>
 #include "timeSystem.h"
 
-void TimeSystem::increaseByMinutes(float& deltaMinutes) {
+bool TimeSystem::increaseByMinutes(float& deltaMinutes) {
 	mDeltaTime += static_cast<unsigned short>(deltaMinutes * mTicksPerSecond);
-	if (mDeltaTime < 1.f) return ;
+	if (mDeltaTime < 1.f) return false;
 
 	unsigned short deltaInMinutes = mDeltaTime;
 	mDeltaTime	-= deltaInMinutes;
 
 	mMinutes	+= deltaInMinutes;
-	mHours		+= mMinutes / 60;
+	if (mMinutes / 60 > 0) mHours += mMinutes / 60, mHourPassed = true;
+	else mHourPassed = false;
 	mDays		+= mHours / 24;
 	mMonths		+= mDays / 30;
 	mYears		+= mMonths / 12;
@@ -35,8 +36,9 @@ void TimeSystem::increaseByMinutes(float& deltaMinutes) {
 			}
 		}
 	}*/
+	return mHourPassed;
 }
 
-std::string TimeSystem::show() {
-	return  std::to_string(mYears) + " : "  + std::to_string(mMonths) + " : " + std::to_string(mDays) + " : " + std::to_string(mHours) + " : " + std::to_string(mMinutes);
+std::string TimeSystem::show() const {
+	return  std::to_string(mHours) + " : " + std::to_string(mMinutes) + '\n' + std::to_string(mDays) + "." + std::to_string(mMonths) + "." + std::to_string(mYears);
 }
--- src/timeSystem.h
+++ src/timeSystem.h
@@ -6,16 +6,17 @@ class TimeSystem
 public:
 	TimeSystem() {}
 
-	void increaseByMinutes(float& deltaMinutes);
-	std::string show();
+	bool increaseByMinutes(float& deltaMinutes);
+	std::string show() const;
 
 private:
-	unsigned short int mMinutes = 0;
-	unsigned short int mHours = 0;
-	unsigned short int mDays = 0;
-	unsigned short int mMonths = 0;
-	unsigned short int mYears = 0;
+	unsigned short int mMinutes = 43;
+	unsigned short int mHours = 6;
+	unsigned short int mDays = 29;
+	unsigned short int mMonths = 6;
+	unsigned short int mYears = 1991;
 	unsigned short int mDeltaTime = 0;
 
-	unsigned short int mTicksPerSecond = 100;
+	unsigned short int mTicksPerSecond = 75;
+	bool mHourPassed = false;
 };
